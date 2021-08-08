3391f69b571e3cf37423e386d328f28c10349c50
taufiqGit <wecannn29@gmail.com>
2021-08-08 14:56:07 +0700

    hariono

--- src/components/load.js
+++ src/components/load.js
@@ -1,8 +1,8 @@
 
 export default function Load(params) {
     return(
-        <div className="w-full h-full flex justify-center items-center">
-            <h2 className="font-Quicksand text-xl text-coklat-muda font-semibold">Load..</h2>
+        <div className="w-full h-full flex justify-center items-center" style={{height: 'calc(100% - 4rem)'}}>
+            <h2 className="font-Quicksand text-xl text-coklat-muda font-semibold">Loading..</h2>
         </div>
     )
 }
